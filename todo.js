e78ccd206b2da508760fb4a3b50b795dbd5b82c5
yongki150 <53007747+yongki150@users.noreply.github.com>
2020-05-02 18:07:43 +0900

    add checking

--- todo.js
+++ todo.js
@@ -1,79 +1,122 @@
 const toDoForm = document.querySelector('.js-toDoForm'),
-toDoInput = toDoForm.querySelector('input'),
-toDoList = document.querySelector('.js-toDoList');
+    toDoInput = toDoForm.querySelector('input'),
+    toDoList = document.querySelector('.js-toDoList');
 
 const TODOS_LS = 'toDos';
 let toDosArr = []; //data
 
-function deleteToDo(event){
+const CHECK = "fa-check-square-o",
+    UNCHECK = "fa-check-square",
+    LINE_THROUGH = "line-through";
+
+function deleteToDo(event) {
     let btn = event.target;
     const li = btn.parentElement.parentElement.parentElement;
-    console.log(li);
-    toDoList.removeChild(li); //html상에만 삭제. 데이터삭제x
-    const cleanToDos = toDosArr.filter(function(toDo){
-        /*
-        여기서 toDo는 toDos의 value값 (toDoObj)
-        toDo.id 는 number형태
-        li.id 는 string형태임
-         */
-        return toDo.id !== parseInt(li.id); // 👍 2 !== "2"
+    toDoList.removeChild(li);
+    const cleanToDos = toDosArr.filter(function (toDo) {
+        console.log(toDo.id, li.id);
+        return toDo.id !== li.id;
     }); //forEach함수처럼 toDosArr 각각에 function실행후 true인 요소로 새로운 arr을 만듬
-    console.log(cleanToDos);
     toDosArr = cleanToDos;
+    console.log(toDosArr);
     saveToDos();
-    
 }
-//toDosArr를 가져와 LS에 저장하는역할
-function saveToDos(){ 
-    localStorage.setItem(TODOS_LS, JSON.stringify(toDosArr)); //로컬스토리지 value에 [object Object]라고 저장됨 ? 로컬스토리지에 JS data를 저장할수 없음. string형태만 가능
-    //JSON.stringify로 object → string형태로 변환해줌! 반대도 가능.
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDosArr));
 }
-function paintToDo(text){
+function paintToDo(task) {
     const li = document.createElement('li'),
-    delBtn = document.createElement('button'),
-    span = document.createElement('span'),
-    newId = toDosArr.length + 1;
-    // li.style.listStyleType = "\1F44D";
-    delBtn.innerHTML = `<span class="trash-hover" style="color: #fff;"><i class="fa fa-trash fa-lg fa-quote-left fa-pull-left fa-border" aria-hidden="true"></i></span>`;
-    // delBtn.innerText = '❌';
+        finBtn = document.createElement('button'),
+        delBtn = document.createElement('button'),
+        span = document.createElement('span');
+
+    const DONE = task.check ? CHECK : UNCHECK;
+    const LINE = task.check ? LINE_THROUGH : '';
+    const COLOR = task.check ? 'lightgray' : '';
+
+    finBtn.classList.add("pl");
+    finBtn.innerHTML = `<span class="hover" style="color: #fff;"><i class="fa ${DONE} fa-lg fa-border" id="${task.id}"></i></span>`;
+    console.log(span);
+
+    span.style.textDecoration = `${LINE}`;
+    li.style.color = `${COLOR}`;
+
+    delBtn.classList.add("pr");
+    delBtn.innerHTML = `<span class="hover" style="color: #fff;"><i class="fa fa-trash de fa-lg fa-border" aria-hidden="true"></i></span>`;
+
+    finBtn.addEventListener('click', function (e) {
+        const target = e.target;
+        const li = e.target.parentElement.parentElement.parentElement;
+        const span = li.children;
+
+        // 기존 클래스에 동일이름 중첩이 된다!!
+        target.classList.toggle(CHECK);
+        target.classList.toggle(UNCHECK);
+        if(target.classList.contains(CHECK)){
+            span[1].style.textDecoration = 'line-through';
+            span[1].style.color = 'lightgray';
+        }else{
+            span[1].style.color = 'inherit';
+            span[1].style.textDecoration = 'none';
+        }
+        li.classList.toggle(LINE_THROUGH);
+        // if (hasClass) {
+        //     target.classList.replace('fa-check-square', 'fa-check-square-o');
+        toDosArr.forEach(function(item){
+            if(item.id === target.id ){
+                item.check = item.check ? false : true;
+            }
+        });
+        console.log(toDosArr);
+        saveToDos();
+    });
     delBtn.addEventListener('click', deleteToDo);
 
-    span.innerText = text;
-    
-    li.appendChild(span);
-    li.appendChild(delBtn); 
+    span.innerText = task.text;
+    li.id = task.id;
 
-    li.id = newId; // 👍 나중에 삭제시 어떤것을 삭제할지 판별
+    li.appendChild(finBtn);
+    li.appendChild(span);
+    li.appendChild(delBtn);
+
     toDoList.appendChild(li);
 
-    const toDosObj = {
-        text : text,
-        id : newId
-    };
-    toDosArr.push(toDosObj); //arr에 요소추가
-    saveToDos();
+    // saveToDos();
 }
 
 //refresh이벤트초기화 및 input되는값에 적용할 함수
-function handleSubmit(event){
+function handleSubmit(event) {
     event.preventDefault();
-    const currentValue = toDoInput.value;
+    const currentValue = taskObj(toDoInput.value);
     paintToDo(currentValue);
-    toDoInput.value = ''; 
-}
-//LS에 저장된 data를 html로 
-function loadToDos(){
-    const loadedToDos = localStorage.getItem(TODOS_LS); // key를 받아옴
-    if(loadedToDos !== null){
-        const parsedToDos = JSON.parse(loadedToDos); // string → object
-        // console.log(typeof(loadedToDos), typeof(parsedToDos));
-        // console.log(parsedToDos);
+    toDoInput.value = '';
 
-        //refresh해도 toDos를 불러와 화면에 저장된상태로 남아있도록
-        parsedToDos.forEach(function(toDo){ paintToDo(toDo.text); })
+    toDosArr.push(currentValue);
+    console.log(toDosArr);
+    saveToDos();
+}
+function taskObj(text) {
+    var random = Math.random() * 10;
+    var newId = random.toFixed(3);
+    return {
+        text: text,
+        id: newId,
+        check: false
     }
 }
-function init(){
+//LS에 저장된 data를 html로 
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS); // key를 받아옴
+    if (loadedToDos !== null) {
+        toDosArr = JSON.parse(loadedToDos); // string → object
+        
+        //refresh해도 toDos를 불러와 화면에 저장된상태로 남아있도록
+    }
+    toDosArr.forEach(function (toDo) { 
+        paintToDo(toDo); 
+    });
+}
+function init() {
     loadToDos();
     toDoForm.addEventListener('submit', handleSubmit);
 }
